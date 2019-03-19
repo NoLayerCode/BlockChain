82dac46fafff266350e13bfac750b52ef0f9031b
NoLayerCode <vyelhekar@gmail.com>
2019-03-20 00:38:31 +0530

    create blockchain

--- Basics/create_blockchain.py
+++ Basics/create_blockchain.py
@@ -24,16 +24,16 @@ class Blockchain:
         self.chain.append(block)
         return block
     
-    def get_prev_block():
+    def get_prev_block(self):
         return self.chain(-1)
     
     def proof_of_work(self, prev_proof):
         new_proof = 1
-        check_proof = false
+        check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
-                check_proof = true
+                check_proof = True
             else:
                 new_proof += 1
         return new_proof
@@ -41,6 +41,69 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_valid(self, chain):
+        prev_block = chain[0]
+        index = 1
+        while index < len(chain):
+            block = chain[index]
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] !='0000':
+                return False
+            prev_block = block
+            index += 1
+        return True
+            
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+    prev_proof = prev_block['proof']
+    proof = blockchain.proof_of_work(prev_proof)
+    prev_hash = blockchain.hash(prev_block)
+    block = blockchain.create_block(proof, prev_hash)
+    response = {'message' : 'block added',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'prev_hash' : block['prev_hash']}
+    return jsonify(response), 200
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
             
             
